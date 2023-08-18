95da488efb52faa6d5c80a014c2003076c3e81ef
Helen Cantu <helen_beatriz02@outlook.com>
2023-08-18 21:28:01 +0000

    Added confirmed payment on stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,32 +1,41 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
 
 def checkout(request):
-    basket = request.session.get('basket', {}) 
-    
-    if not basket:
-        messages.error(request, "There's nothing in your basket at the moment") 
-        return redirect('products')  # You can use the view name directly
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    if request.method == 'POST':
-        order_form = OrderForm(request.POST)
-        
-        if order_form.is_valid():
-            # Save the order form data and process the order
-            order = order_form.save()
-            # Perform other order processing tasks
-            messages.success(request, 'Your order has been placed successfully!')
-            return redirect('order_success')  # Redirect to a success page
-    else:
-        order_form = OrderForm()
-    
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, "There's nothing in your basket at the moment")
+        return redirect(reverse('products'))
+
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51NgYEQGETKbO2ZE5YsXJUNVYwFVgQV1fX8FIntiAxl92EAPCRjE9g9VJ7EGD0gW3fjJtJ5C0UCXaVKhw0nFflWFc00ZtgaXf5u',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
