4153223d4525a01be7da913e53cb790111a7f94d
Helen Cantu <helen_beatriz02@outlook.com>
2023-08-20 17:01:58 +0000

    Added profile form and customization

--- users/views.py
+++ users/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def users(request):
     """ Display the user's profile. """
     users = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=users)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=users)
+    orders = users.orders.all()
+
     template = 'users/users.html'
     context = {
-        'users': users,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
