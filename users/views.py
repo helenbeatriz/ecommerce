0fe0624baf135ee5c1a4d2505bb4c179e1ff9a56
Helen Cantu <helen_beatriz02@outlook.com>
2023-08-20 16:37:21 +0000

    Update allauth templates and user profile

--- users/views.py
+++ users/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def users(request):
     """ Display the user's profile. """
+    users = get_object_or_404(UserProfile, user=request.user)
 
     template = 'users/users.html'
-    context = {}
+    context = {
+        'users': users,
+    }
 
     return render(request, template, context)
