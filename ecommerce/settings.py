3ef3703bd284ed467cb886dd1b4f971a107fd54b
Helen Cantu <helen_beatriz02@outlook.com>
2023-08-22 22:32:39 +0000

    Added static media fix

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -174,7 +174,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/' 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if 'DEVELOPMENT' in os.environ:
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
